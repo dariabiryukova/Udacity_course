90a41336d1f12e2cfcdbd501b530a9fdf060d13c
daria biryukova <biryukovadi@gmail.com>
2014-10-22 12:20:37 +0100

    Tests were simplyfied

---
+++ app/src/androidTest/java/com/example/bivy/sunshine/TestDB.java
@@ -0,0 +1,124 @@
+package com.example.bivy.sunshine;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import com.example.bivy.sunshine.data.WeatherContract;
+import com.example.bivy.sunshine.data.WeatherDBHelper;
+
+/**
+ * Created by bivy on 17/10/14.
+ */
+public class TestDB extends AndroidTestCase {
+
+    public static final String LOG_TAG = TestDB.class.getSimpleName();
+
+    public void testCreateDb() throws Throwable {
+        mContext.deleteDatabase(WeatherDBHelper.DATABASE_NAME);
+        SQLiteDatabase db = new WeatherDBHelper(
+            this.mContext).getWritableDatabase();
+        assertEquals(true, db.isOpen());
+        db.close();
+    }
+
+    public void testInsertReadDb() {
+
+        // If there's an error in those massive SQL table creation Strings,
+        // errors will be thrown here when you try to get a writable database.
+        WeatherDBHelper dbHelper = new WeatherDBHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues testValues = createNorthPoleLocationValues();
+
+        long locationRowId;
+        locationRowId = db.insert(WeatherContract.Location.TABLE_NAME, null, testValues);
+
+        // Verify we got a row back.
+        assertTrue(locationRowId != -1);
+        Log.d(LOG_TAG, "New row id: " + locationRowId);
+
+        // Data's inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
+        // the round trip.
+
+        // A cursor is your primary interface to the query results.
+        Cursor cursor = db.query(
+            WeatherContract.Location.TABLE_NAME,  // Table to Query
+            null, // all columns
+            null, // Columns for the "where" clause
+            null, // Values for the "where" clause
+            null, // columns to group by
+            null, // columns to filter by row groups
+            null // sort order
+        );
+
+        validateCursor(cursor, testValues);
+
+        // Fantastic.  Now that we have a location, add some weather!
+        ContentValues weatherValues = createWeatherValues(locationRowId);
+
+        long weatherRowId = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, weatherValues);
+        assertTrue(weatherRowId != -1);
+
+        // A cursor is your primary interface to the query results.
+        Cursor weatherCursor = db.query(
+            WeatherContract.WeatherEntry.TABLE_NAME,  // Table to Query
+            null, // leaving "columns" null just returns all the columns.
+            null, // cols for "where" clause
+            null, // values for "where" clause
+            null, // columns to group by
+            null, // columns to filter by row groups
+            null  // sort order
+        );
+
+        validateCursor(weatherCursor, weatherValues);
+
+        dbHelper.close();
+    }
+
+    static ContentValues createWeatherValues(long locationRowId) {
+        ContentValues weatherValues = new ContentValues();
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DATETEXT, "20141205");
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DEGREES, 1.1);
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, 1.2);
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, 1.3);
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MAX_TEMP, 75);
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MIN_TEMP, 65);
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, 5.5);
+        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_ID, 321);
+
+        return weatherValues;
+    }
+
+    static ContentValues createNorthPoleLocationValues() {
+        // Create a new map of values, where column names are the keys
+
+        ContentValues testValues = new ContentValues();
+        testValues.put(WeatherContract.Location.COLUMN_LOCATIONSETTINGS, "99705");
+        testValues.put(WeatherContract.Location.COLUMN_CITY_NAME, "North Pole");
+        testValues.put(WeatherContract.Location.COLUMN_LOCATION_LAT, 64.7488);
+        testValues.put(WeatherContract.Location.COLUMN_LOCATION_LONG, -147.353);
+
+        return testValues;
+    }
+
+    static void validateCursor(Cursor valueCursor, ContentValues expectedValues) {
+
+        assertTrue(valueCursor.moveToFirst());
+
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet) {
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            assertFalse(idx == -1);
+            String expectedValue = entry.getValue().toString();
+            assertEquals(expectedValue, valueCursor.getString(idx));
+        }
+        valueCursor.close();
+    }
+
+}
